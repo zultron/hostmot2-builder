a5c45e0a2bd843a375290c76de54fc3fe874837f
John Morris <john@zultron.com>
2017-07-07 12:52:50 -0500

    README:  output readability tweaks
    print banner with `********` delimiters for major operations

--- doit.sh
+++ doit.sh
@@ -3,8 +3,8 @@
 CURDIR="$PWD"
 TMPDIR13="$CURDIR/unpack/13"
 TARBALL13="$CURDIR/Xilinx_ISE_DS_Lin_13.4_O.87xd.3.0.tar"
-TMPDIR9="$CURDIR/unpack/9"
-TARBALL9="$CURDIR/ISE_DVD_92i.tar.gz"
+TMPDIR10="$CURDIR/unpack/10"
+TARBALL10="$CURDIR/ISE_DS.tar"
 HM2FW_DIR="${CURDIR}/hostmot2-firmware"
 HM2FW_URL="https://github.com/LinuxCNC/hostmot2-firmware.git"
 SUITE="ISE_DS"
@@ -12,93 +12,101 @@ LICENSE_FILE="${CURDIR}/Xilinx/13.4/${SUITE}/EDK/data/core_licenses/Xilinx.lic"
 
 
 
+banner() {
+    echo
+    echo "***********************************************************************"
+    if test -n "$*"; then
+	echo "***** ${*}"
+	echo "***********************************************************************"
+    fi
+}
+
 install-ise-13() {
     # Install Xilinx ISE 13.4
     if ! test -f "${CURDIR}/Xilinx/13.4/${SUITE}/.xinstall/install.log" || \
 	    ! grep -q 'Xilinx Installer/Updater exited with return status "0"' \
 	      "${CURDIR}/Xilinx/13.4/${SUITE}/.xinstall/install.log"; then
 
-	# Unpack tarball into temp directory
+	banner "Unpack Xilinx ISE 13.4 tarball"
 	if ! test -f "${TMPDIR13}/.unpacked" && \
 		! test -f "${CURDIR}/Xilinx/13.4/${SUITE}/.xinstall/install.log"; then
-	    echo "Unpacking tarball:"
-	    echo "    '${TARBALL13}'"
-	    echo "    -> '${TMPDIR13}'"
+	    echo "    Unpacking '${TARBALL13}'"
+	    echo "        into '${TMPDIR13}'"
 	    mkdir -p "${TMPDIR13}"
 	    tar xCf "${TMPDIR13}" "${TARBALL13}" --strip-components=1
 	    touch "${TMPDIR13}/.unpacked"
 	else
-	    echo "Xilinx tarball already unpacked"
+	    echo "    Xilinx tarball already unpacked"
 	fi
 
-	# Run automated installer
+	banner "Running Xilinx ISE 13.4 batch install"
 	# - Original config file generated with:
 	#   $ unpack/bin/lin64/batchxsetup -samplebatchscript ise-install-13.conf
 	#   - Add `eula_accepted=Y` for completely automated install
-	echo "Running Xilinx 13.4 installer"
 	"${TMPDIR13}/bin/lin64/batchxsetup" -batch "${CURDIR}/ise-install-13.conf"
     else
-	echo "Xilinx 13.4 ${SUITE} already installed"
+	banner "Unpack Xilinx ISE 13.4 tarball"
+	echo "    Xilinx 13.4 ${SUITE} already installed"
     fi
     
     # Add license to license file
+    banner "Add Xilinx ISE 13.4 license to license file"
     if ! grep -q 'PACKAGE ISE_WebPACK xilinxd' "${LICENSE_FILE}"; then
 	if test -f "${CURDIR}/Xilinx.lic"; then
-	    echo "Adding license:"
 	    echo "    from '${CURDIR}/Xilinx.lic'"
 	    echo "    to '${LICENSE_FILE}'"
 	    cat "${CURDIR}/Xilinx.lic" >> "${LICENSE_FILE}"
 	else
-	    echo "ERROR:  No license file found in ${CURDIR}/Xilinx.lic!"
+	    echo "ERROR:  No license file found in '${CURDIR}/Xilinx.lic'!"
+	    exit 1
 	fi
     else
 	echo "License already present in ${LICENSE_FILE}"
     fi
 }
 
-install-ise-9() {
-    # Install Xilinx ISE 9.2
-    if ! test -f "${CURDIR}/Xilinx/9.2i/.xinstall/install.log" || \
+install-ise-10() {
+    # Install Xilinx ISE 10.1
+    if ! test -f "${CURDIR}/Xilinx/10.1/.xinstall/install.log" || \
 	    ! grep -q 'summary= Batch install completed' \
-	      "${CURDIR}/Xilinx/9.2i/.xinstall/install.log"; then
+	      "${CURDIR}/Xilinx/10.1/.xinstall/install.log"; then
 
-	# Unpack tarball into temp directory
-	if ! test -f "${TMPDIR9}/.unpacked" && \
-		! test -f "${CURDIR}/Xilinx/9.2i/.xinstall/install.log"; then
-	    echo "Unpacking tarball:"
-	    echo "    '${TARBALL9}'"
-	    echo "    -> '${TMPDIR9}'"
-	    mkdir -p "${TMPDIR9}"
-	    tar xCf "${TMPDIR9}" "${TARBALL9}" --strip-components=1
-	    touch "${TMPDIR9}/.unpacked"
+	banner "Unpack Xilinx ISE 10.1 tarball"
+	if ! test -f "${TMPDIR10}/.unpacked" && \
+		! test -f "${CURDIR}/Xilinx/10.1/.xinstall/install.log"; then
+	    echo "    Unpacking '${TARBALL10}'"
+	    echo "    -> '${TMPDIR10}'"
+	    mkdir -p "${TMPDIR10}"
+	    tar xCf "${TMPDIR10}" "${TARBALL10}" --strip-components=1
+	    touch "${TMPDIR10}/.unpacked"
 	else
-	    echo "Xilinx tarball already unpacked"
+	    echo "    Xilinx tarball already unpacked"
 	fi
 
-	# Run automated installer
+	banner "Running Xilinx ISE 10.1 batch install"
 	# - See notes in file
-	echo "Running Xilinx 9.2 installer"
-	"${TMPDIR9}/bin/lin64/setup" -batch "${CURDIR}/ise-install-9.conf"
+	"${TMPDIR10}/bin/lin64/setup" -batch "${CURDIR}/ise-install-10.conf"
     else
-	echo "Xilinx 9.2 already installed"
+	echo "    Xilinx 10.1 already installed"
     fi
 }
 
 clone-hm2-fw() {
-    # Clone hostmot2-firmware into current directory
+    banner "Clone hostmot2-firmware"
     if ! test -d "${HM2FW_DIR}/.git"; then
-	echo "Cloning hotmot2-firmware:"
 	echo "    from ${HM2FW_URL}"
 	echo "    to ${HM2FW_DIR}"
 	git clone "${HM2FW_URL}" "${HM2FW_DIR}"
     else
-	echo "Firmware already cloned into directory ${HM2FW_DIR}"
+	echo "    Firmware already cloned into directory ${HM2FW_DIR}"
     fi
 }
 
 
-echo "Starting install on $(date)"
+banner "Starting install on $(date)"
+banner
 install-ise-13
-install-ise-9
+install-ise-10
 clone-hm2-fw
-echo "Finished install on $(date)"
+banner
+banner "Finished install on $(date)"
